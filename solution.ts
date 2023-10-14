7ad8189cb78ad71a44f50c973fd9050268fdb0cd
Kooreis <kooreis.official@gmail.com>
2023-10-14 12:15:32 -0500

    This update adds the 'push' method to the MinStack class. This method pushes a number into the stack array. If the minStack array is empty or the number is less than or equal to the current minimum, the number is also pushed into the minStack array.

--- solution.ts
+++ solution.ts
@@ -1,9 +1,6 @@
-class MinStack {
-    private stack: number[];
-    private minStack: number[];
-
-    constructor() {
-        this.stack = [];
-        this.minStack = [];
+push(x: number): void {
+        this.stack.push(x);
+        if (this.minStack.length === 0 || x <= this.getMin()) {
+            this.minStack.push(x);
+        }
     }
-}
